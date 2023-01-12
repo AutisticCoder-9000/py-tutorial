7ee6af99d6aeace3ff356190e93b5f38f1c9b8f6
AutisticCoder-9000 <117058073+AutisticCoder-9000@users.noreply.github.com>
2023-01-12 08:56:06 +0530

    Update filter_function.py

--- python_normal/filter_function.py
+++ python_normal/filter_function.py
@@ -10,3 +10,11 @@ l=[1,2,6,3,5,3,7]
 print(list(filter(gret4,l)))
 # for use with lambda-------------
 print(list(filter(lambda a:a>4,l)))
+#to mimic the fuction of filtr replace it by the following:
+def my_filter(asd, l):
+	a=[]
+	for i in l:
+		if asd(i)==True:
+			a.append(i)
+	return a
+print(my_filter(gret4,l))
